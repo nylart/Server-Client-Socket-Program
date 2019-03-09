074f9bf7526d3480ce6f874c351f5c8df34c6f3c
Tralyn <let1@unlv.nevada.edu>
2019-03-08 22:32:23 -0800

    Started working on main.cpp

---
+++ Server/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+const string localhostIP = "127.0.0.1";
+const int defaultPort = 5000;
+
+int main(int argc, char *argv[])
+{
+    if ( argc != 3 ){
+        cout<< "Error: Expecting format: ./main ADDRESS PORT";
+    }
+    else{
+
+
+    }
+}
---
+++ Server/server.h
@@ -0,0 +1,4 @@
+#ifndef SERVER_H_SFH9W7WB
+#define SERVER_H_SFH9W7WB
+
+#include <boost/asio.hpp>
